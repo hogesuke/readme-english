2b4c160413145d44373ce4834c877a9dec8d8b45
hogesuke <miyado@gmail.com>
2017-03-26 16:10:43 +0900

    単語のカウント処理のパフォーマンス改善

--- lib/word-counter/word_counter.rb
+++ lib/word-counter/word_counter.rb
@@ -3,19 +3,25 @@ require 'pry'
 require 'benchmark'
 
 class WordCounter
+  def initialize
+    @counts = {}
+  end
+
   def run
     paths = file_paths
-    words = []
 
     paths.each do |path|
       puts "[path: #{path}] processiong"
-      text = read(path)
-      words.concat(extract_words(text))
+
+      text  = read(path)
+      words = extract_words(text)
+      count_words(words)
+
       puts "[path: #{path}] completed"
     end
 
-    result = count_words(words)
-    dump(result)
+    sort
+    dump
   end
 
   private
@@ -42,47 +48,42 @@ class WordCounter
     clean_words
   end
 
+  def count_words(words)
+    words.each do |w|
+      if @counts.has_key?(w)
+        @counts[w] += 1
+      else
+        @counts[w] = 1
+      end
+    end
+  end
+
   def is_rejection?(word)
     return true if word.empty?
-    return true if word.match(/^[=\/a-z]$/)
-    return true if word.match(/^\d+$/)
+    return true if word.match(/^[\d.,-]+$/)
     return true if word.match(/https?:\/\//)
-    return true if word.match(/[|│├└]/)
+    return true if word.match(/[|│├└=\/\\]/)
     return true if word.match(/\p{Hiragana}/)
     return true if word.match(/\p{Katakana}/)
     return true if word.match(/[一-龠々]/)
-    return true if word.match(/.+[.=&<>(){}\/"'@;].+/)
+    return true if word.match(/.+[.&<>(){}"'*@\$:;~].+/)
     return true if word.start_with?('<', '{', '[', '`', '+', '-', '_', '@')
     return true if word.end_with?('>', '}', ']', '`')
     false
   end
 
   def clean(word)
-    word = word.gsub(/^[("':;\-&%#]+/, '')
-    word = word.gsub(/[.,!?)"':;\-&%#]+$/, '')
+    word = word.gsub(/^[.("':;\-~*&%#\$]+/, '')
+    word = word.gsub(/[.,!?)"':;\-~*&%#\$]+$/, '')
     word.downcase
   end
 
-  def count_words(words)
-    uniq_words = words.uniq
-    result = {}
-
-    uniq_words.each do |target|
-      puts "[count: #{target}] processing"
-      time = Benchmark.realtime do
-        count = 0
-        words.each { |w| count += 1 if w == target }
-        words.delete(target)
-        result[target] = count
-      end
-      puts "[count: #{target}] completed #{time}s"
-    end
-
-    Hash[ result.sort_by { |(_, v)| -v } ]
+  def sort
+    @counts = Hash[ @counts.sort_by { |(_, v)| -v } ]
   end
 
-  def dump(result)
-    YAML.dump(result, File.open(File.expand_path('../../../out/result.yaml', __FILE__), 'w'))
+  def dump
+    YAML.dump(@counts, File.open(File.expand_path('../../../out/result.yaml', __FILE__), 'w'))
   end
 end
 
